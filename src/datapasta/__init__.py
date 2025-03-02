9fe9e49648497aa0e25f88e2fac162f4c9cd19f3
Louis Maddox <lmmx@users.noreply.github.com>
2025-03-02 22:43:33 +0000

    Create __init__.py

---
+++ src/datapasta/__init__.py
@@ -0,0 +1,3 @@
+from .main import clipboard_to_pandas, clipboard_to_polars, text_to_pandas, text_to_polars
+
+__all__ = ['clipboard_to_pandas', 'clipboard_to_polars', 'text_to_pandas', 'text_to_polars']
